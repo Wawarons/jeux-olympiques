9308a3ab3d5496c5bb27c69193c203aa74f9d700
Adrien <75527727+Wawarons@users.noreply.github.com>
2024-05-25 00:45:37 +0200

    Update apiService.ts
    Add missing import for axios

--- src/utils/apiService.ts
+++ src/utils/apiService.ts
@@ -1,3 +1,5 @@
+import axios from "axios";
+
 export const checkTokenValidity = (token: string): Promise<boolean> => {
   return axios
     .get(`${import.meta.env.VITE_API_URL}/token/user/validate?token=${token}`, {
